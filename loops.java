136b7e1be9a44a742857853274872629ff4a8be0
Mandeep <voltrontvp@gmail.com>
2023-03-13 11:44:19 +0530

    adding do while loop

--- loops.java
+++ loops.java
@@ -2,13 +2,33 @@ public class loops
 {
 public static void main(String args[])
  {
-// printing first 10 natural number.
+// printing first 10 natural number using while loop.
+/*
 int n = 10;
 int a = 1;
 while(a<=n){
 System.out.println(a);
 a++;
 }
+*/
+
+// printing first 10 natural number using do while loop.
+
+int z = 10;
+int y = 1;
+while{
+System.out.println(y);
+y++;
+}(y<=z)
+
+
+
+
+
+
+
+
+
 
  }
 }

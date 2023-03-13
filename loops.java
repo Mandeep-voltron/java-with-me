2dd5500ac59b5acedc7b446c6a31566fc9ab5ba6
Mandeep <voltrontvp@gmail.com>
2023-03-13 11:33:18 +0530

    changes in loops file

---
+++ loops.java
@@ -0,0 +1,14 @@
+public class loops
+{
+public static void main(String args[])
+ {
+// printing first 10 natural number.
+int n = 10;
+int a = 1;
+while(a<=n){
+System.out.println(a);
+a++;
+}
+
+ }
+}
